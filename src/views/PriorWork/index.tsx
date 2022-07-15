267d9f1aeab7acc9d14c3a183aff5c1c9b74b8b5
Jack Gilcrest <me@jp4g.llc>
2022-07-15 14:17:18 -0400

    add battlezips to prior work page

--- src/views/PriorWork/index.tsx
+++ src/views/PriorWork/index.tsx
@@ -3,6 +3,7 @@ import Rotate from 'react-reveal/Rotate';
 import { createUseStyles } from 'react-jss';
 import Flex from 'components/Flex';
 import Typography from 'components/Typography';
+import battlezips from 'assets/images/battlezips.png';
 import blitz from 'assets/images/blitz.svg';
 import rigor from 'assets/images/rigor.svg';
 import YouTube from 'react-youtube';
@@ -69,6 +70,42 @@ export default function PriorWork(): JSX.Element {
         </Rotate>
         <Rotate bottom right>
           <div className={styles.card} style={{ margin: '32px 0px 0px auto' }}>
+            <Flex alignItems='center' gap='16px'>
+              <img alt='BattleZips Privacy Stack' className={styles.image} src={battlezips} />
+              <Typography variant='text3'>BattleZips Privacy Stack</Typography>
+            </Flex>
+            <Typography
+              paragraph
+              variant='body1'
+              style={{ color: '#10995A', marginTop: '8px' }}
+            >
+              <b>
+                Won 6th out of 171 participating teams at <a href="https://dorahacks.io/grant/ethdenver22/top">
+                  EthDenver 2022
+                </a>
+              </b>
+            </Typography>
+            <YouTube
+              opts={{ height: '250px', width: '100%' }}
+              style={{ marginTop: '24px' }}
+              videoId='wAFJ1mJO0d0'
+            />
+            <Typography paragraph style={{ marginTop: '24px' }} variant='body1'>
+              <a href="https://github.com/jp4g/battlezips">BattleZips</a> is a demonstration of Zero Knowledge cryptography applied
+              to layer 1 EVM contracts to play hidden information games in public
+              execution environments. Written in Solidity, SnarkJS, and Circom,
+              this stack was built as a demonstration to ourselves and others of
+              our capability in applying zero knowledge to arbitrary business logic.
+              BattleZips Privacy Stack presented February 2022 at EthDenver.
+              <br />
+              <br />
+              Services: Zero Knowledge, Cryptography, Smart Contracts, Subgraphs,
+              Metatransactions, IPFS, R&D
+            </Typography>
+          </div>
+        </Rotate>
+        <Rotate left top>
+          <div className={styles.card} style={{ margin: '32px' }}>
             <Flex alignItems='center' gap='16px'>
               <img alt='Rigor' className={styles.image} src={rigor} />
               <Typography variant='text3'>Rigor</Typography>
