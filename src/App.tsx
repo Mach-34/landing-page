965534404eb9460957a749c0f178cd5918e743ad
Ian-Bright <ianbrighton1@gmail.com>
2022-08-11 00:08:25 -0600

    reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,18 @@ import { ThemeProvider } from 'react-jss';
 import MediaProvider from 'contexts/MediaContext';
 import { theme } from 'theme';
 import { createBrowserHistory } from 'history';
-import { useEffect } from 'react';
 
 const browserHistory = createBrowserHistory();
 
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_TRACKING_ID ?? '');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+browserHistory.listen((location: any) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
 function App() {
-  ReactGA.initialize(process.env.REACT_APP_GOOGLE_TRACKING_ID ?? '');
-
-  browserHistory.listen((location: any) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  });
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <MediaProvider>
       <ThemeProvider theme={theme}>
