377ddb2fcb53be86d95df5f8b68b197f7023eb10
Ian-Bright <ianbrighton1@gmail.com>
2022-10-28 15:05:32 -0600

    repo is public

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,6 @@ import MediaProvider from 'contexts/MediaContext';
 import { theme } from 'theme';
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_TRACKING_ID ?? '');
-// ReactGA.send({
-//   hitType: 'pageview',
-//   page: window.location.pathname + window.location.search,
-// });
 
 function App() {
   return (
